1ac3907707608d14b6c316d3c08c75e4b7965d29
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2022-06-08 13:18:33 -0400

    initial commit

---
+++ src/main/java/com/CRUD/Spring_Boot_ToDoApp_Backend/SpringBootToDoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.CRUD.Spring_Boot_ToDoApp_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToDoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToDoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CRUD/Spring_Boot_ToDoApp_Backend/SpringBootToDoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUD.Spring_Boot_ToDoApp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
